6439137ff9899c9f37c109b8c1ebedfdaf39717f
0786ShivaniBansal <sb8877517@gmail.com>
2021-12-26 16:35:41 +0530

    degugging

--- mydjango/settings.py
+++ mydjango/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
+    'hello',
 ]
 
 MIDDLEWARE = [
@@ -118,12 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'STATIC')
-    
-    
-]
-STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+STATICFILES_DIRS=[ BASE_DIR / 'static',]
+
+STATIC_ROOT= BASE_DIR / 'assets'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
